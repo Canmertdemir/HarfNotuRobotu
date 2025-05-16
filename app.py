7078c027b2ec2a3814a77c7dd5109ca6791fd371
Can <canmertdemir2@gmail.com>
2025-05-16 16:43:49 +0300

    Condition conflict removed.

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
+def quiz_not_kontrol(data):
+    for i in range(1, 5):
+        quiz_key = f'quiz{i}'
+        if not (0 <= data[quiz_key] <= 100):
+            return False, f"{quiz_key} skoru 0 ile 100 arasında olmalıdır."
+    return True, ""
+def vize_final_not_kontrol(data):
+    if not (0 <= data['vize'] <= 100):
+        return False, "Vize skoru 0 ile 100 arasında olmalıdır."
+    if not (0 <= data['final'] <= 100):
+        return False, "Final skoru 0 ile 100 arasında olmalıdır."
+    return True, ""
+
 @app.route('/')
 def anasayfa():
     return render_template('home.html')
@@ -12,14 +25,25 @@ def home():
 
 @app.route('/quiz-ortalama', methods=['POST'])
 def quiz_ortalama():
+
     data = request.get_json()
+
+    gecerli , mesaj = quiz_not_kontrol(data)
+    if not gecerli:
+        return jsonify({'Hata!': mesaj})
+
     q_avg = sum([data['quiz1'], data['quiz2'], data['quiz3'], data['quiz4']]) / 4
     return jsonify({'quiz_ortalama': round(q_avg, 2)})
 
 @app.route('/genel-ortalama', methods=['POST'])
 def genel_ortalama():
     data = request.get_json()
-    quiz_avg = sum([data['quiz1'], data['quiz2'], data['quiz3'], data['quiz4']]) / 4
+
+    gecerli , mesaj = vize_final_not_kontrol(data)
+    if not gecerli:
+        return jsonify({'Hata!': mesaj})
+
+    quiz_avg = quiz_ortalama()
     genel = quiz_avg * 0.1 + data['vize'] * 0.3 + data['final'] * 0.6
     harf = 'AA' if genel >= 90 else 'BA' if genel >= 85 else 'BB' if genel >= 75 else 'CC'
     return jsonify({'genel_ortalama': round(genel, 2), 'harf_notu': harf})
@@ -27,9 +51,17 @@ def genel_ortalama():
 @app.route('/devamsizlik', methods=['POST'])
 def devamsizlik():
     data = request.get_json()
-    toplam_saat = data['haftalik_ders_sayisi'] * 14 * (data['teorik_saat'] + (data['uygulama_saat'] if data['uygulamali_mi'] else 0))
-    limit = toplam_saat * 0.3
-    durum = "Geçerli" if data['devamsizlik'] <= limit else "Devamsızlıktan Kaldı"
+
+    if  data['uygulamali'] == 0:
+        toplam_devamsizlik_hakki = (15 * data['teorik']) * 0.4
+    else:
+        toplam_devamsizlik_hakki = (15 * data['uygulama']) * 0.3
+
+    if data['devamsizlik'] > toplam_devamsizlik_hakki:
+        durum = "Devamsızlıktan kaldı."
+    else:
+        durum = "Derse devaalı."
+
     return jsonify({'durum': durum})
 
 if __name__ == '__main__':
